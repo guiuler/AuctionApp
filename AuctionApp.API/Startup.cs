43a93dd64bff74ff6be00e754f9da7eca7a404f1
unknown <grmoliveira93@gmail.com>
2018-12-17 20:44:50 -0200

    Revertendo o projeto para build local

--- AuctionApp.API/Startup.cs
+++ AuctionApp.API/Startup.cs
@@ -83,14 +83,7 @@ namespace AuctionApp.API
             // app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseMvc(routes => {
-                routes.MapSpaFallbackRoute(
-                    name: "spa-fallback",
-                    defaults: new { controller = "Fallback", action = "Index"}
-                );
-            });
+            app.UseMvc();
         }
     }
 }
